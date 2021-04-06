cef62c9241abbd15f72b9c889d17aa9bbfd84137
msramalho <19508417+msramalho@users.noreply.github.com>
2021-04-06 02:42:36 +0200

    feat: final touches

--- docs/search.js
+++ docs/search.js
@@ -3,7 +3,8 @@ $(function () {
 	let data = false;
 	$.getJSON("hashed_data.json", function (json) {
 		data = json.name;
-		$("#loading").html("dados carregados (para Portugal apenas)")
+		$("#loading").remove()
+		$("#loaded").show()
 	});
 
 	// Auto-selection when clicked
@@ -23,12 +24,11 @@ $(function () {
 
 	$('.search').keyup(function () {
 		let v = smart_phone(this.value);
-		console.log(v)
 
 		if (data !== false && v !== false) {
 			let hash = _hash(v);
 			if (hash in data) {
-				display(`deste já deu: <i>${data[hash].replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}</i> :(`);
+				display(`deste já deu: <i style='color:#FF5733'>${data[hash].replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}</i> :(`);
 				return;
 			}
 		}
